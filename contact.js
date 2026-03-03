42b8ec8bbb5b261de07fe95630d3975393f85860
truptic21 <truptic238@gmail.com>
2026-03-03 13:05:21 +0530

    add contact page

---
+++ contact.js
@@ -0,0 +1,29 @@
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = form.querySelector('input[type="text"]').value;
+  const email = form.querySelector('input[type="email"]').value;
+  const phone = form.querySelector('input[type="tel"]').value;
+  const design = form.querySelector("select").value;
+  const message = form.querySelector("textarea").value;
+
+  const whatsappMessage =
+    `Hello Aariyansh Aari Work 👋\n\n` +
+    `Name: ${name}\n` +
+    `Email: ${email}\n` +
+    `Mobile: ${phone}\n` +
+    `Design: ${design}\n` +
+    `Message: ${message}`;
+
+  const whatsappNumber = "919175604825";
+
+  const whatsappURL =
+    `https://wa.me/${whatsappNumber}?text=` +
+    encodeURIComponent(whatsappMessage);
+
+  window.open(whatsappURL, "_blank");
+
+  form.reset();
+});
